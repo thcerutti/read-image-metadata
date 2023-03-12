0892a1f87ba7dea06bcd01a3cba0f98123e098ed
Thiago Cerutti <thiago.cerutti@rdstation.com>
2023-03-11 22:04:14 -0300

    chore: refactoring

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const { getInputParams, validateInputParams } = require("./src/inputParams");
 const { processFileMetadata } = require("./src/processFileMetadata");
+const { writeFileSync } = require("fs");
 
 const inputParams = getInputParams();
 const errors = validateInputParams(inputParams);
@@ -8,4 +9,23 @@ if (errors.length > 0) {
   return;
 }
 
-processFileMetadata(inputParams)
+const logFileName = "output.log";
+
+try {
+  processFileMetadata(inputParams).then((metadataContent) => {
+    console.log(metadataContent);
+    console.info(`[${metadataContent.length} files processed]`);
+
+    const formattedContent = metadataContent
+      .map((item) => item.data.parameters.split("\n")[0])
+      .join("\n\r");
+    writeFileSync(inputParams.output, formattedContent);
+    console.info(`[results have been written to '${inputParams.output}' file]`);
+  });
+  throw "eeepaaaaa";
+} catch (error) {
+  writeFileSync(`./${logFileName}`, error);
+  console.error(
+    `Houston, we have a problem! Logs were saved to ./${logFileName}}`
+  );
+}
--- src/processFileMetadata.js
+++ src/processFileMetadata.js
@@ -1,7 +1,7 @@
 var exifr = require("exifr");
 const { readdirSync, writeFileSync } = require("fs");
 
-const processFileMetadata = (inputParams) => {
+const processFileMetadata = async (inputParams) => {
   const files = readdirSync(inputParams.path);
 
   const readFilePromises = files.map(async (fileName) => ({
@@ -9,19 +9,7 @@ const processFileMetadata = (inputParams) => {
     data: await exifr.parse(`${inputParams.path}/${fileName}`),
   }));
 
-  Promise.all(readFilePromises).then((data) => {
-    const extractedInfo = data.map((info) => ({
-      fileName: info.fileName,
-      prompt: info.data[inputParams.prop],
-    }));
-    console.info(`[${extractedInfo.length} files processed]`);
-    const formattedContent = extractedInfo
-      .map((item) => item.prompt.split("\n")[0])
-      .join("\n\r");
-    console.info(extractedInfo);
-    writeFileSync(inputParams.output, formattedContent);
-    console.info(`[results have been written to '${inputParams.output}' file]`);
-  });
+  return await Promise.all(readFilePromises);
 };
 
 module.exports = { processFileMetadata };
