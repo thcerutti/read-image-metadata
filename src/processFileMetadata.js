a91c59ddd57214c171416d26bbf800e0f0c15085
Thiago Cerutti <thiago.cerutti@rdstation.com>
2023-03-11 21:46:31 -0300

    fix: write out only parameters first line

--- src/processFileMetadata.js
+++ src/processFileMetadata.js
@@ -6,23 +6,22 @@ const processFileMetadata = (inputParams) => {
 
   const readFilePromises = files.map(async (fileName) => ({
     fileName: `${inputParams.path}/${fileName}`,
-    data: await exifr.parse(`${inputParams.path}/${fileName}`)
+    data: await exifr.parse(`${inputParams.path}/${fileName}`),
   }));
 
-
   Promise.all(readFilePromises).then((data) => {
-    const extractedInfo = data.map(info => ({
+    const extractedInfo = data.map((info) => ({
       fileName: info.fileName,
-      prompt: info.data[inputParams.prop]
-    }))
-    console.info(`[${extractedInfo.length} files processed]`)
-    console.log(extractedInfo.map(item => item.prompt).join("\n\r"));
-    // writeFileSync(inputParams.output, JSON.stringify(extractedInfo, null, 2))
-    writeFileSync(inputParams.output, extractedInfo.map(item => item.prompt).join("\n\r"))
-
-    console.info(`[results have been written to '${inputParams.output}' file]`)
-  })
-
+      prompt: info.data[inputParams.prop],
+    }));
+    console.info(`[${extractedInfo.length} files processed]`);
+    const formattedContent = extractedInfo
+      .map((item) => item.prompt.split("\n")[0])
+      .join("\n\r");
+    console.info(extractedInfo);
+    writeFileSync(inputParams.output, formattedContent);
+    console.info(`[results have been written to '${inputParams.output}' file]`);
+  });
 };
 
 module.exports = { processFileMetadata };
